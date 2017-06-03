c80c125621cb6dc0b32b3266e4b28692d4742101
TheButlerDidIt <labutler@gmail.com>
2017-06-03 14:11:32 -0400

    renders board

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+var rows = 40;
+var columns = 40;
+var $row = $("<div />", {
+    class: 'row'
+});
+var $square = $("<div />", {
+    class: 'square'
+});
+
+function render(){
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    for (var j = 0; j < rows; j++) {
+        $("#board").append($row.clone());
+    }
+}
+
+render();
+
 });
