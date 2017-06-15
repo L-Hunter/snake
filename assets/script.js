751badee686645833c3c786d36a3e77172216fa8
TheButlerDidIt <labutler@gmail.com>
2017-06-14 22:35:38 -0400

    adds movement

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
 
 //add controls
+var character = document.getElementById("character");
 document.addEventListener("keydown", movement);
+var x = 405;
+var y = 405;
+character.style.left = x + "px";
+character.style.top = y + "px";
 
 function movement(direction){
     var key = direction.keyCode;
     if(key === 38){
         console.log("up");
+        y -= 22;
+        character.style.top = y + 'px';
+    	return y;
     } else if (key === 37) {
     	console.log("left");
+    	x -= 22;
+    	character.style.left = x + 'px';
+    	return x;
     } else if (key === 39){
     	console.log("right");
+    	x += 22;
+    	character.style.left = x + 'px';
+    	return x;
     } else if (key === 40) {
-    	console.log("down")
+    	console.log("down");
+    	y += 22;
+    	character.style.top = y + 'px';
+    	return y;
     } else {
         alert("Please use the arrow keys");
     }
